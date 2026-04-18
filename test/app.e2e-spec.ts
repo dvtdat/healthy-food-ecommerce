0088d7316f6dc67d6c836fa356b1ce55e61f78cd
anhvd6 <anhvd6@vng.com.vn>
2026-04-18 22:14:20 +0700

    fix: fix e2e test setup and TypeScript types for jest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,13 @@
 import { Test, type TestingModule } from '@nestjs/testing';
 import type { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import type { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +16,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!!!!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/products (GET) returns 200', () => {
+    return request(app.getHttpServer()).get('/products').expect(200);
   });
 });
